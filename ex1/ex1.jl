eaa9cafc7eda924cf19d85c936f50847247cf8a3
Administrator <admin@example.com>
2015-10-29 17:35:13 +0200

    Plotting added

--- ex1/ex1.jl
+++ ex1/ex1.jl
@@ -1,48 +1,69 @@
-#read data
-data = readdlm("ex1data1.txt", ',');
+using PyPlot
 
-# set X
-X = data[:,1]
-X = hcat(ones(length(X)), X)
+#=
+		Linear regression example (Single input)
+		requires PyPlot from Python
 
-#set Y
+		Editor used : Atom 1.0.19
+=#
+
+#= Training =#
+
+# Readining data from ex1data1.txt file
+data = readdlm("ex1data1.txt", ',')
+# Desired output
 Y = data[:,2]
-
-
-# count
+# Inputs
+X = data[:,1]
+X = hcat(ones(length(X)), X) # [ones(length(X)), X] did not worked. I dont know why. (Installed Julia today)
+# Count
 m = length(Y)
-
-#alpha 
+# Learning rate
 alpha = 0.01
-
-# init theta
+# Parameters initiated
 theta = zeros(2,1)
+# Iteration
+iter = 1500
 
-
-function predict(x, theta)	
+# predict
+function predict(x, theta)
 	return x * theta
 end
 
+# Calculating gradient descent algorithm to find optimal thetas
 function gradientDescent(x, y, theta, alpa, m)
 	error = predict(x, theta) - y
 	der = error' * x
 	return theta - ((alpha / m) * der')
 end
 
+# Train
 function train(x, y, theta, alpha, m)
-	for i = 1:1500
+	for i = 1:iter
 		theta = gradientDescent(x, y, theta, alpha, m)
 	end
-	
+
 	return theta
 end
 
+#= Testing =#
 
+# Train to find thetas
 theta = train(X, Y, theta, alpha, m)
 
+# Test thetas
 predict1 = [1 3.5] * theta;
 predict2 = [1 7] * theta;
 
 println(theta)
 println(predict1*10000)
 println(predict2*10000)
+
+#= Plotting =#
+
+# Draw regression function
+PyPlot.plot(X[:, 2], Y, "rx")
+PyPlot.draw()
+PyPlot.plot(X[:, 2], X * theta, "-")
+PyPlot.draw()
+PyPlot.show()
